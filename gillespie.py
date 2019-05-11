d13d6d3064f7a1e90e255b1fcfbbba598f529799
Trevor <trevmanz94@gmail.com>
2019-05-11 23:38:33 +0100

    create class for model and gillespie algorithm. refactor statistics computations.

--- gillespie.py
+++ gillespie.py
@@ -1,125 +1,162 @@
-from __future__ import division
 import numpy as np
-#import matplotlib.pyplot as pyplot
-import time
-from tabulate import tabulate
 import pandas as pd
-#from numba import njit
+import matplotlib.pyplot as pyplot
+from tabulate import tabulate
+from numba import njit
+import random
+import time
 
 
-delta=np.array([[1, -1, 0, 0, 0, 0],
-                [0, 0, 1, -1, 0, 0],
-                [0, 0, 0, 0, 1, -1]])
+class mRNADynamicsModel:
+    def __init__(self, alpha, tau1, tau2, tau3, lambd, delta):
+        self.alpha = alpha
+        self.tau1 = tau1
+        self.tau2 = tau2
+        self.tau3 = tau3
+        self.lambd = lambd
+        self.delta = delta
+        self.labels = ["T factor", "mRNA 1", "mRNA 2"]
 
-N=1000000
-T=np.zeros([1,N])
-X=np.zeros([delta.shape[0],N])
-tsteps=np.zeros([1,N])
-t=0
+    def run_gillespe(self, N=100_000_000, start_at_ss=True):
+        # If we start the simulation from the predicted steady states,
+        # we will not need to wait for the initial rise in TF/mRNA
+        # levels and our simulation therefore converges much more quickly
+        x = self.compute_theoretical()[0] if start_at_ss else np.ones(3)
+        self.X, self.T, self.tsteps = fast_gillespe(
+            np.array(x),
+            self.alpha,
+            self.tau1,
+            self.tau2,
+            self.tau3,
+            self.lambd,
+            self.delta,
+            N,
+        )
 
-alpha=10
-tau1=2
-tau2=2
-tau3=4
-lambd=10
+    def compute_theoretical(self):
+        # Theoretical states
+        x1 = self.alpha * self.tau1
+        x2 = self.lambd * self.tau2 * x1
+        x3 = self.lambd * self.tau3 * x1
 
-#theoretical steady states
-x1ss=alpha*tau1
-x2ss=lambd*tau2*x1ss
-x3ss=lambd*tau3*x1ss
+        # Theoretical variance and covariances
+        v1 = 1 / x1
+        v2 = 1 / x2 + self.tau1 / (x1 * (self.tau1 + self.tau2))
+        v3 = 1 / x3 + self.tau1 / (x1 * (self.tau1 + self.tau3))
 
-##Theoretical (from FDT) variances and covariances
-var1=1/x1ss
-var2=1/x2ss + tau1/(x1ss*(tau1+tau2))
-var3=1/x3ss + tau1/(x1ss*(tau1+tau3))
+        cov12 = self.tau1 / (x1 * (self.tau1 + self.tau2))
+        cov13 = self.tau1 / (x1 * (self.tau1 + self.tau3))
+        cov23 = (
+            self.tau1
+            * (2 * self.tau2 * self.tau3 + self.tau1 * (self.tau2 + self.tau3))
+        ) / (
+            x1
+            * (self.tau1 + self.tau2)
+            * (self.tau1 + self.tau3)
+            * (self.tau2 + self.tau3)
+        )
 
-cov12=tau1/(x1ss*(tau1+tau2))
-cov13=tau1/(x1ss*(tau1+tau3))
-cov23=(tau1*(2*tau2*tau3+tau1*(tau2+tau3)))/(x1ss*(tau1+tau2)*(tau1+tau3)*(tau2+tau3))
+        return [x1, x2, x3], [v1, v2, v3], [cov12, cov13, cov23]
+
+    def collect_stats(self):
+        # Theoretical
+        Xss, Xvars, Xcovs = self.compute_theoretical()
+
+        # Mean and Variance Calculations
+        # Time weight the mean
+        tw_means = (self.X * self.tsteps).sum(1) / self.tsteps.sum()
+        # mean Errors
+        p_err = (Xss - tw_means) / Xss * 100
+
+        # Variance and Covariance calculations
+        # Time weight the variance
+        diffs = self.X - tw_means[:, np.newaxis]
+        tw_vars = (self.tsteps * diffs ** 2).sum(1) / self.tsteps.sum()
+        # Calculate the weighted time-weighted variance
+        tw_w_vars = tw_vars / tw_means ** 2
+        combs = ((0, 1), (0, 2), (1, 2))  # Covariance combinations
+        # Calculate the weighted time-weighted covariance
+        tw_w_covs = [
+            ((self.tsteps * (diffs[c[0]]) * (diffs[c[1]])).sum() / np.sum(self.tsteps))
+            / (tw_means[c[0]] * tw_means[c[1]])
+            for c in combs
+        ]
+
+        labs = [f"var({lab})" for lab in self.labels]
+        labs += [f"cov({self.labels[c[0]]}, {self.labels[c[1]]})" for c in combs]
+        fdts = np.array(Xvars + Xcovs)
+        all_covs = np.array(list(tw_w_vars) + tw_w_covs)
+
+        df1 = pd.DataFrame(
+            {
+                "component": self.labels,
+                "predicted_mean": Xss,
+                "gillespie_mean": tw_means,
+                "percent_error": p_err,
+            }
+        )
+
+        df2 = pd.DataFrame(
+            {
+                "value": labs,
+                "fdt": fdts,
+                "gillespie": all_covs,
+                "error": (fdts - all_covs) / fdts * 100,
+            }
+        )
+
+        return df1, df2
+
+    def plot_X_trace(self):
+        pyplot.plot(self.T, self.X[0], label="Transcription factor")
+        pyplot.plot(self.T, self.X[1], label="mRNA 1")
+        pyplot.plot(self.T, self.X[2], label="mRNA 2")
+        pyplot.xlabel("Unitless Time")
+        pyplot.ylabel("# of molecules")
+        pyplot.legend(loc="best")
+
+    def plot_flux_hist(self):
+        R1p = np.full((1, len(self.X[0])), self.alpha)[0]
+        R1m = self.X[0] / self.tau1
+        R2p = self.lambd * self.X[0]
+        R2m = self.X[1] / self.tau2
+        R3p = R2p
+        R3m = self.X[2] / self.tau3
+        pyplot.hist((R1p - R1m), 100, facecolor="red", alpha=0.5)
+        pyplot.hist((R2p - R2m), 100, facecolor="green", alpha=0.3)
+        pyplot.hist((R3p - R3m), 100, facecolor="blue", alpha=0.3)
 
 
-#If we start the simulation from the predicted steady states, we will not need to wait for the initial rise in TF/mRNA levels and our simulation therefore converges much more quickly
-x=np.array([x1ss,x2ss,x3ss])
-randoms=np.random.rand(N,2)
+@njit
+def fast_gillespe(x, alpha, tau1, tau2, tau3, lambd, delta, N):
+    t = 0
+    T = np.zeros(N)
+    tsteps = np.zeros(N)
+    X = np.zeros((delta.shape[0], N))
+    for i in range(N):
+        rates = np.array(
+            [alpha, x[0] / tau1, lambd * x[0], x[1] / tau2, lambd * x[0], x[2] / tau3]
+        )
+        summed = np.sum(rates)
+        tau = (-1) / summed * np.log(random.random())
+        t = t + tau
+        T[i] = t
+        tsteps[i] = tau
 
-#
+        reac = np.sum(np.cumsum(np.true_divide(rates, summed)) < random.random())
+        x = x + delta[:, reac]
+        X[:, i] = x
 
-start=time.clock()
-for i in range(N):
-        rates=[alpha, x[0]/tau1, lambd*x[0], x[1]/tau2, lambd*x[0], x[2]/tau3]
-        tau=(-1)/np.sum(rates)*np.log(randoms[i,0])
-        t=t+tau
-        reac=np.sum(np.cumsum(np.true_divide(rates,np.sum(rates)))<randoms[i,1])
-        x=x+delta[:,reac]
-        T[:,i]=t
-        X[:,i]=x
-        tsteps[:,i]=tau
-end=time.clock()
-print("Gillespie took "+str(end-start)+" seconds")
+    return X, T, tsteps
 
 
-
-def plotting():
-    pyplot.plot(T[0],X[0],label='Transcription factor')
-    pyplot.plot(T[0],X[1],label='mRNA 1')
-    pyplot.plot(T[0],X[2],label='mRNA 2')
-    pyplot.xlabel('Unitless Time')
-    pyplot.ylabel('# of molecules')
-    pyplot.legend()
-
-#plotting()
-
-#X=np.delete(X,range(100000),1) #removal of the initial rise
-#T=np.delete(T,range(100000),1)
-#tsteps=np.delete(tsteps,range(100000),1)
-
-## fluxes
-R1p=np.full((1,len(X[0])),alpha)[0]
-R1m=X[0]/tau1
-R2p=lambd*X[0]
-R2m=X[1]/tau2
-R3p=R2p
-R3m=X[2]/tau3
-
-def histograms():
-    pyplot.hist((R1p-R1m), 100, facecolor='red', alpha=0.5)
-    pyplot.hist((R2p-R2m), 100, facecolor='green', alpha=0.3)
-    pyplot.hist((R3p-R3m), 100, facecolor='blue', alpha=0.3)
-
-#histograms()
-
-#Defining the weighted mean, variance and covariance functions
-
-def tw_mean(series): #time weight the mean
-    return np.sum(series*tsteps)/np.sum(tsteps)
-
-def tw_var(series): #time weight the variance
-    return np.sum(tsteps*(series-tw_mean(series))**2)/np.sum(tsteps)
-
-def tw_w_var(series): #calculate the weighted time-weighted variance
-    return tw_var(series)/(tw_mean(series)**2)
-
-def tw_cov(series1,series2): #calculate the time-weighted covariance
-    return np.sum(tsteps*(series1-tw_mean(series1))*(series2-tw_mean(series2)))/np.sum(tsteps)
-
-def tw_w_cov(series1,series2): #calculate the weighted time-weighted covariance
-    return tw_cov(series1,series2)/(tw_mean(series1)*tw_mean(series2))
-
-def results_table_means():
-    print(tabulate([["Transcription factor", x1ss,tw_mean(X[0]),(x1ss-tw_mean(X[0]))/x1ss * 100],
-                    ["mRNA 1", x2ss,tw_mean(X[1]),(x2ss-tw_mean(X[1]))/x2ss * 100],
-                    ["mRNA 2", x3ss,tw_mean(X[2]),(x3ss-tw_mean(X[2]))/x3ss * 100]],
-                    ["Component", "Predicted mean", "Gillespie mean"," %Error"], tablefmt="grid"))
-
-results_table_means()
-
-def results_table_cov():
-    print(tabulate([["var(T factor)", var1,tw_w_var(X[0]),(var1-tw_w_var(X[0]))/var1 * 100],
-                    ["var(mRNA 1)", var2,tw_w_var(X[1]),(var2-tw_w_var(X[1]))/var2 * 100],
-                    ["var(mRNA 2)", var3,tw_w_var(X[2]),(var3-tw_w_var(X[2]))/var3 * 100],
-                    ["cov(T fact, mRNA 1)", cov12,tw_w_cov(X[0],X[1]),(cov12-tw_w_cov(X[0],X[1]))/cov12 * 100],
-                    ["cov(T fact, mRNA 2)", cov13,tw_w_cov(X[0],X[2]),(cov13-tw_w_cov(X[0],X[2]))/cov13 * 100],
-                    ["cov(mRNA 1, mRNA 2)", cov23,tw_w_cov(X[1],X[2]),(cov23-tw_w_cov(X[1],X[2]))/cov23 * 100]],
-                    ["Value", "FDT", "Gillespie"," %Error"], tablefmt="grid"))
-
-results_table_cov()
+if __name__ == "__main__":
+    random.seed(42)
+    D = np.array([[1, -1, 0, 0, 0, 0], [0, 0, 1, -1, 0, 0], [0, 0, 0, 0, 1, -1]])
+    print(
+        "Intialising model and running gillespie algorithm for 100,000,000 iteratations.\n"
+    )
+    model = mRNADynamicsModel(alpha=10, tau1=2, tau2=2, tau3=4, lambd=10, delta=D)
+    model.run_gillespe()
+    for table in model.collect_stats():
+        print(tabulate(table, headers="keys", showindex=False), "\n")
