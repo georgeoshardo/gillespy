6835805b2180337d377d18ba5fd372d4eb5c03f6
Trevor <trevmanz94@gmail.com>
2019-05-14 13:32:10 +0100

    comment parallel optimisations. move body into __main__.

--- multi_sim.py
+++ multi_sim.py
@@ -3,27 +3,30 @@ import numpy as np
 import pandas as pd
 from tabulate import tabulate
 
+# Require Dask for running independent simulations in parallel
 from dask import compute, delayed
 import dask.multiprocessing
 from dask.diagnostics import ProgressBar
 ProgressBar().register()
 
-from gillespie import mRNADynamicsModel as M
+# Require model for multiple simulations
+from gillespie import mRNADynamicsModel as M 
 
-random.seed(42)
-
-D = np.array([[1, -1, 0, 0, 0, 0], [0, 0, 1, -1, 0, 0], [0, 0, 0, 0, 1, -1]])
-
-model = M(alpha=10, tau1=2, tau2=2, tau3=4, lambd=10, delta=D)
-
-taus = [0.1, 1, 3, 5, 50]
-alphas = [0.1, 10, 100]
-lambds = [0.1, 1, 5, 10, 50]
-
-
-@delayed  # Dask decorator to define tasks
+@delayed  # Used to delay execution until scheduled on thread by Dask
 def gather_stats(model, tau1, alpha, lambd, N=100_000_000):
-    # Change model parameters and run Gillespie
+    """Gather statistics from Gillespie with different model parameters.
+    
+    Args: 
+        model:
+        tau1 (float): transcription factor lifetime
+        alpha (float): transcription factor birth rate
+        lambd (float): mRNA birth rate
+        N (int): Number of Gillespie iterations (default=100_000_000)
+
+    returns: Tuple of mean and var/covars DataFrames for Gillespie sim
+
+    """
+    # Adjust model parameters and run Gillespie
     model.tau1 = tau1
     model.alpha = alpha
     model.lambd = lambd
@@ -31,26 +34,43 @@ def gather_stats(model, tau1, alpha, lambd, N=100_000_000):
 
     # Collect statistics
     means, covs = model.collect_stats()
+
+    # Add columns to DataFrames for model params used in sim
     means["tau1"] = tau1
     means["alpha"] = alpha
     means["lambd"] = lambd
     covs["tau1"] = tau1
     covs["alpha"] = alpha
     covs["lambd"] = lambd
+
     return means, covs
 
+if __name__ == "__main__":
+    random.seed(42)
 
-# Create dask graph of desired simulations
-values = [
-    gather_stats(model, t, a, lam) for t in taus for a in alphas for lam in lambds
-]
+    # Diffusion matrix
+    D = np.array([[1, -1, 0, 0, 0, 0], [0, 0, 1, -1, 0, 0], [0, 0, 0, 0, 1, -1]])
 
-# Execute task graph, running independent simulations in parrallel
-results = compute(*values, scheduler="processes")
+    # Intialise model
+    model = M(alpha=10, tau1=2, tau2=2, tau3=4, lambd=10, delta=D)
 
-# Concatonate means and covariances dataframes
-all_means = pd.concat([r[0] for r in results])
-all_covs = pd.concat([r[1] for r in results])
+    # Parameters to test
+    taus = [0.1, 1, 3, 5, 50]
+    alphas = [0.1, 10, 100]
+    lambds = [0.1, 1, 5, 10, 50]
 
-all_means.to_csv('all_means.csv', index=False)
-all_covs.to_csv('all_covs.csv', index=False)
+    # Create dask graph of desired simulations
+    values = [
+        gather_stats(model, t, a, lam) for t in taus for a in alphas for lam in lambds
+    ]
+
+    # Execute task graph, running independent simulations in parrallel via Dask
+    results = compute(*values, scheduler="processes")
+
+    # Concatonate means and covariances dataframes
+    all_means = pd.concat([r[0] for r in results])
+    all_covs = pd.concat([r[1] for r in results])
+
+    # Write data to files
+    all_means.to_csv('all_means.csv', index=False)
+    all_covs.to_csv('all_covs.csv', index=False)
